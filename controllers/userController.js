517257f1a5e438ae68471015085abafcf1d07da3
MahendrSH <maahi2001sh@gmail.com>
2023-05-14 23:19:04 +0530

    user controllers js registore user get All users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,49 @@
 const userModels = require("../models/userModels");
 const CatchAsycErrors =require("../middleware/CatchAsyncError");
+
+
+// auth user conrollers 
+//  registor users 
+const registor = CatchAsycErrors(async (req, res, next) => {
+    const { name, email, password } = req.body;
+    const user = await userModels.create({
+        name,
+        email,
+        password,
+    
+    });
+    res.status(201).json({  
+        
+        success: true,
+        user,
+    });
+});
+
+
+
+//  admin
+// get all users 
+const getAllUsers = CatchAsycErrors(async (req, res, next) => {
+    const users = await userModels.find();
+    res.status(200).json({
+        success: true,
+        users,
+    });
+}
+
+);
+
+// admin 
+// get user by id 
+
+const getUserById = CatchAsycErrors(async (req, res, next) => {
+    const user = await userModels.findById(req.params.id);
+    if (!user) {
+        return next(new ErrorHandler("user not found", 404));
+    }
+    res.status(200).json({
+        success: true,
+        user,
+    });
+});
+
